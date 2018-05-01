6646ec1295d874bb335110317fa711259b35a935
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 07:03:59 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,10 +15,10 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/login' do
-    if logged_in?
-      redirect '/tweets'
-    else
+    if !logged_in?
       erb :'users/login'
+    else
+      redirect '/tweets'
     end
   end
 
