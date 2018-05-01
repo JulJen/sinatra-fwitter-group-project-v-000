edde71e190c61422e1bb676bcb96490302c26fdc
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 07:21:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -102,8 +102,8 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/tweets/:id' do
-    @tweet = Tweet.find_by_id(params[:id])
     if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
       erb :'tweets/show_tweet'
     else
       redirect '/login'
