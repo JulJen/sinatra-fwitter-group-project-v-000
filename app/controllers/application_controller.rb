10a1382620270ebc865d6fb52e07f40e39a64049
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 05:16:33 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,7 +68,22 @@ class ApplicationController < Sinatra::Base
     end
   end
 
-
+  post '/tweets' do
+    if logged_in?
+      if params[:content] == ""
+        redirect to '/tweets/new'
+      else
+        @tweet = current_user.tweets.build(content: params[:content])
+        if @tweet.save
+          redirect to "/tweets/#{@tweet.id}"
+        else
+          redirect to '/tweets/new'
+        end
+      end
+    else
+      redirect to '/login'
+    end
+  end 
 
 
   helpers do
