6d73463fbda9a34de538eec6b23735c59b4ccada
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 06:32:58 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ActiveRecord::Base
   has_many :tweets
   has_secure_password
 
+  def slug
+    self.username.gsub(" ", "-").downcase
+    # obj = self.name.downcase
+    # obj.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|name| name.slug == slug}
+  end
+
+
 end
