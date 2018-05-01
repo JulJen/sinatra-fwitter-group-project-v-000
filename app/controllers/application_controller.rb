995da6797bc96303a03178b7b5808d2149a38d1c
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 07:00:02 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -68,7 +68,7 @@ class ApplicationController < Sinatra::Base
   get '/logout' do
     if logged_in?
       session.clear
-      redirect '/login'
+      redirect '/tweets'
     else
       redirect '/'
     end
