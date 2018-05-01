5695223d9a122873c0bdad43166e581c83f0c41a
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 06:43:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,9 +21,9 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(:username => params[:username])
 
-    if user && user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = user.id
-      erb  :'tweets/tweets'
+      redirect '/tweets'
     else
       redirect '/failure'
     end
