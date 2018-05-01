10aa7da63eecf00dc01dd0dffae59d6e3b9c9ee3
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 05:05:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -81,7 +81,8 @@ class ApplicationController < Sinatra::Base
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user
+      # User.find_by_id(session[:user_id])
     end
   end
 
