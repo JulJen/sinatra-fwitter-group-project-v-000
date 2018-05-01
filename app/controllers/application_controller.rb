49a0c7ebb12a583aaa720d56198693081c7ab775
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 04:19:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < Sinatra::Base
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect "/account"
+      redirect 'tweets/tweets'
     else
       redirect "/failure"
     end
