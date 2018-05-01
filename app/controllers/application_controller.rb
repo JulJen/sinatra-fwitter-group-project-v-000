99688a3a7b4e36b211a2dc5ee2b0ee4f57a6425a
Jul Jen <juliajen.prog@gmail.com>
2018-05-01 04:09:44 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,9 +23,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
-    #your code here
-
-    user = User.new(:username => params[:username], :password => params[:password])
+    @user = User.new(:username => params[:username], :password => params[:password])
 
     if user.username == ""
       redirect "/failure"
